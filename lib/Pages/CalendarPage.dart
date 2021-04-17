1397eb47a135cd5aa9a3f405b5957a416f52d978
edaUstun <edaustun1141@gmail.com>
2021-04-17 23:47:53 +0300

    the calendar

--- lib/Pages/CalendarPage.dart
+++ lib/Pages/CalendarPage.dart
@@ -1,24 +1,52 @@
 import 'package:flutter/material.dart';
 import 'package:walker/Widgets/constants.dart';
 import 'package:walker/Widgets/navigation_drawer.dart';
+import 'package:table_calendar/table_calendar.dart';
 
-//import 'package:walker/Widgets/constants.dart';
 class CalendarPage extends StatefulWidget {
   @override
   _CalendarPageState createState() => _CalendarPageState();
 }
 
 class _CalendarPageState extends State<CalendarPage> {
+  CalendarFormat _calendarFormat = CalendarFormat.month;
+  DateTime _selectedDay = DateTime.now();
+  DateTime _focusedDay = DateTime.now();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: kPrimaryColor,
       drawer: NavBar(),
       appBar: AppBar(
-        title: Text("Calendar Page"),
+        title: Text("Calendar"),
         backgroundColor: kPrimaryColor,
         centerTitle: true,
       ),
+     body: TableCalendar(
+       firstDay: DateTime.utc(2010, 10, 16),
+       lastDay: DateTime.utc(2030, 3, 14),
+       focusedDay: DateTime.now(),
+
+       selectedDayPredicate: (day) {
+         return isSameDay(_selectedDay, day);
+       },
+       onDaySelected: (selectedDay, focusedDay) {
+         setState(() {
+           _selectedDay = selectedDay;
+           _focusedDay = focusedDay; // update `_focusedDay` here as well
+         });
+       },
+       calendarFormat: _calendarFormat,
+       onFormatChanged: (format) {
+         setState(() {
+           _calendarFormat = format;
+         });
+       },
+       onPageChanged: (focusedDay) {
+         _focusedDay = focusedDay;
+       },
+
+     ),
     );
   }
 }
